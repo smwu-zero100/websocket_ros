46b340402561c1a3aaef1bedefbf2d5c58e10c1c
Jeong Boin <54930076+boing-86@users.noreply.github.com>
2023-02-14 13:23:55 +0900

    [Update] Publisher 추가
    Publish 추가함!

--- websocket_test/src/websocket_pubtest.py
+++ websocket_test/src/websocket_pubtest.py
@@ -9,28 +9,29 @@ from std_msgs.msg import String
 
 class Test():
 	def __init__(self):
-		# self.data = ''
 		rospy.init_node('websocket_server')
 		rate = rospy.Rate(10)
-		#string_pub = rospy.Publisher('/websocket_test', String, queue_size = 1)
-		start_server = websockets.serve(Test.accept, "0.0.0.0", 0000)
+		string_pub = rospy.Publisher('/websocket_test', String, queue_size = 1)
+		start_server = websockets.serve(Test.accept, "0.0.0.0", 9090)
 		
 		asyncio.get_event_loop().run_until_complete(start_server)
 		rospy.loginfo("listening")
-		
-		#string_msg = String()
-		#jsonObject = json.loads(self.data.encode('utf-8').decode('utf-8')))
-		#string_msg.data = jsonObject.get(name)
-		
-		#string_pub.publish(string_msg)
 		asyncio.get_event_loop().run_forever()
+
 		rospy.spin()
 			
 	async def accept(websocket, path):
 		while True:
 			data = await websocket.recv()
-			print(data)
-			#await websocket.send("echo" + data)
+			json_obj = json.loads(data.decode('utf-8'))
+
+			# async 라서 global 변수는 안쓰이는 것 같음
+			# 어쩔 수 없이 accept 에다가 때려박자...
+			string_pub = rospy.Publisher('/websocket_test', String, queue_size = 1)
+			string_msg = String()
+			string_msg.data = json_obj['name']
+			string_pub.publish(string_msg)
+			rospy.loginfo(string_msg.data)
 
 
 if __name__ == '__main__':
